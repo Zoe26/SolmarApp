5c7c1be4b1a334139e404f5a659742b44bae524c
Luis Calua <luis.calua@idslatam.com>
2016-11-09 09:13:45 -0500

    Codebar Scanner Button Send

--- app/src/main/java/com/idslatam/solmar/View/Fragments/HomeFragment.java
+++ app/src/main/java/com/idslatam/solmar/View/Fragments/HomeFragment.java
@@ -37,4 +37,8 @@ public class HomeFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
 
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+    }
 }
